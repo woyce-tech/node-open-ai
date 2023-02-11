1e3d7bd34442689644a8ca2db8f4db19162ce4b5
Shyamsinh Parmar <parmarshyamsingh8@gmail.com>
2023-02-11 18:15:20 +0530

    #update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,10 +9,15 @@ const openai = new OpenAIApi(configuration);
 async function runCompletion () {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "best way to learn coding",
+      prompt: "who is shyamsinh parmar and where is he?",
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
-    console.log(completion.data);
-    // console.log(completion.data.choices[0].text);
+    // console.log(completion.data);
+    console.log(completion.data.choices[0].text);
     }
     
     runCompletion();
